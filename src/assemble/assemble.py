e852a2440e950dad0252467b342efdfd6385a2c8
Otto Kohulak <pravod@gmail.com>
2024-01-17 15:44:22 +0100

    WIP

--- src/assemble/assemble.py
+++ src/assemble/assemble.py
@@ -1,7 +1,43 @@
 import sys
 import glob
+import re
 
 to_stdout = True
+
+class Csv_info:
+
+    def __init__(self):
+        self.csvs = []
+
+    def __call__(self, iopt, lines):
+        data = dict(
+        angmom = None,
+        type_ = None,
+        normalized = None,
+        angtype = None,
+        multiplicity = None,
+        npar = None)
+
+        for line in lines:
+            for k in data:
+                print(k, line.strip())
+                m = re.match(r'^\s*-\s+'+ k.replace("_", "") +'\s*=\s*(.+)\s*$', line)
+                if m:
+                    print("match")
+                    data[k] = m.group(1)
+
+        if data['angmom'] is None:
+            return
+        if data['npar'] is None:
+            return
+        if data['multiplicity'] is None:
+            return
+
+        data['iopt'] = iopt
+
+        self.csvs.append(data)
+
+csv_info = Csv_info()
 
 # Load header
 with open('makefun_header.f90', 'r') as f:
@@ -17,7 +53,18 @@ orb_files = glob.glob('orb_*.f90')
 # read files
 orbitals = {f.replace("orb_", "").replace(".f90",""): open(f, 'r').read() for f in orb_files}
 
-# Ensamlble makefun.f90
+# Read comment lines in orbitals
+for k, v in orbitals.items():
+    lines = v.split('\n')
+    comment_lines = []
+    for i, line in enumerate(lines):
+        if line.strip().startswith('!'):
+            comment_lines.append(line.strip()[1:])
+        else:
+            break
+    csv_info(k, comment_lines)
+
+# Assemble makefun_out.f90
 with open('makefun_out.f90', 'w') as f:
     if to_stdout:
         f = sys.stdout
@@ -27,4 +74,12 @@ with open('makefun_out.f90', 'w') as f:
         f.write(f'case ({k})\n')
         f.write(v)
     f.write(footer)
+
+# Assemble makefun_out.csv
+print(csv_info.csvs)
+header = "iopt,angmom,type_,normalized,angtype,multiplicity,npar"
+with open('makefun_out.csv', 'w') as f:
+    f.write(f'{header.replace("-","")}\n')
+    for csv in csv_info.csvs:
+        f.write(','.join([str(csv[x]) for x in header.split(",")]) + '\n')
 
--- src/assemble/orb_36.f90
+++ src/assemble/orb_36.f90
@@ -1,31 +1,25 @@
-
-
+  ! p orbital
+  ! 
+  ! - angmom = 1
+  ! - type = Gaussian
+  ! - normalized = yes
+  ! - angtype = spherical
+  ! - npar = 1
+  ! - multiplicity = 3
+  !
 
   dd1=dd(indpar+1)
-
-  !\print *, "i0, indtmin, indtm"
-  !\print *,i0, indtmin, indtm
-
-
-  !        if(iflagnorm.gt.2) then
-  !        c=dsqrt(2.d0)*pi**(-0.75d0)*(2.d0*dd1)**1.25d0
   c=dd1**1.25d0*1.42541094070998d0
-  !        endif
-
 
   do k=indtmin,indtm
     distp(k,1)=c*dexp(-dd1*r(k)**2)
   end do
 
-  !        indorbp=indorb
-  !
   do ic=1,3
-    !           if(iocc(indshell+ic).eq.1) then
     indorbp=indorb+ic
     do i=i0,indtm
       z(indorbp,i)=rmu(ic,i)*distp(i,1)
     end do
-    !           endif
   end do
 
   if(typec.ne.1) then
@@ -33,23 +27,16 @@
     fun=-2.d0*dd1*distp(0,1)
     fun2=fun*(1.d0-2.d0*dd1*r(0)**2)
 
-    !              indorbp=indorb
-
     do ic=1,3
-      !                if(iocc(indshell+ic).eq.1) then
       indorbp=indorb+ic
       do i=1,3
-        z(indorbp,indt+i)=rmu(ic,0)*rmu(i,0)*                        &
-            fun
+        z(indorbp,indt+i)=rmu(ic,0)*rmu(i,0)*fun
       end do
       z(indorbp,indt+ic)=z(indorbp,indt+ic)+fun0
       z(indorbp,indt+4)=rmu(ic,0)*(4.d0*fun+fun2)
-      !                 endif
     end do
-    !endif for indt
   end if
 
   indpar=indpar+1
   indshell=indshell+3
   indorb=indorbp
-
